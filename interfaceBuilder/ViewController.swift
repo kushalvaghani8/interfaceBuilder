d4941d3cfd965fa007b5ab71b4f165a54edfec94
kushal <kushal_vaghani@yahoo.com>
2022-01-24 18:54:04 -0500

    Initial Commit

---
+++ interfaceBuilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  interfaceBuilder
+//
+//  Created by Kushal Vaghani on 24/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    print("test")
+    }
+
+
+}
+
